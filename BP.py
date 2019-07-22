c89e5dbaee2d31644e05c84630e6be080afd90d2
wzx <rucwzx@126.com>
2019-07-21 21:10:03 -0400

    fix mistake

--- BP.py
+++ BP.py
@@ -41,16 +41,17 @@ class Neuoron_layer:
 
     def update_param(self, y, layer='output', back_delta=None, back_weights=None, learning_rate=0.01, momentum=0):
         if layer == 'output':
-            self.delta = (y - self.output) * dsigmoid(self.a)
+            self.delta = (y - self.output)
         else:
             self.delta = np.dot(back_delta, back_weights.T) * dsigmoid(self.a)
-        b_add = learning_rate * np.sum(self.delta, axis=0) / self.delta.shape[0]
+        b_add = learning_rate * np.average(self.delta, axis=0)
         self.b = self.b + b_add + momentum * self.last_b_add
         self.last_b_add = b_add
-        w_add = learning_rate * np.dot(self.input.T, self.delta) / self.delta.shape[0]
+        w_add = learning_rate * \
+                np.average(np.array([self.input[i][:, None]@self.delta[i][None, :] for i in range(self.input.shape[0])]),axis=0)
         self.w += w_add + momentum * self.last_w_add
         self.last_w_add = w_add
-        return self.w, self.delta
+        return self.w-w_add, self.delta
 
 
 class Neuoron_network:
@@ -60,40 +61,43 @@ class Neuoron_network:
 
     def forward(self, x, y):
         result = x
-        for i in xrange(len(self.network)):
+        for i in range(len(self.network)):
             result = self.network[i].calc(result)
 
-    def backward(self, x, y, momentum):
+    def backward(self, x, y, learning_rate, momentum):
         back_weights, back_delta = self.network[-1].update_param(y, momentum=momentum)
-        for i in xrange(2, self.num_layer):
-            back_weights, back_delta = self.network[-i].update_param(y, layer='hider', back_delta=back_delta,
-                                                                     back_weights=back_weights, momentum=momentum)
+        for i in range(2, self.num_layer):
+            back_weights, back_delta = self.network[-i].update_param(y, layer='hidden', back_delta=back_delta,
+                                                                     back_weights=back_weights,
+                                                                     learning_rate=learning_rate,
+                                                                     momentum=momentum)
 
     def fit(self, x, y, epoch_num):
-        for j in xrange(epoch_num):
+        for j in range(epoch_num):
             self.forward(x, y)
             self.backward(x, y)
 
-    def fit_SGD(self, train_data, train_target, test_data, test_target, learning_rate, min_batch_size=100, epoch_num=10,
-                momentum=0.9):
+    def fit_SGD(self, train_data, train_target, learning_rate, min_batch_size=100, epoch_num=10,
+                momentum=0):
         shuffle = np.random.choice(train_data.shape[0], train_data.shape[0], replace=False)
         train_data = train_data[shuffle]
         train_target = train_target[shuffle]
-        for i in xrange(epoch_num):
-            for j in xrange(train_target.shape[0] / min_batch_size):
+        for i in range(epoch_num):
+            for j in range(train_target.shape[0] // min_batch_size):
                 self.forward(train_data[j:j + min_batch_size], train_target[j:j + min_batch_size])
-                self.backward(train_data[j:j + min_batch_size], train_target[j:j + min_batch_size], momentum=momentum)
-            self.emulate(test_data, test_target)
+                self.backward(train_data[j:j + min_batch_size], train_target[j:j + min_batch_size],
+                              learning_rate=learning_rate, momentum=momentum)
+            #self.emulate(test_data, test_target)
 
     def predict(self, x):
         result = x
-        for i in xrange(len(self.network)):
+        for i in range(len(self.network)):
             result = self.network[i].calc(result)
         return result
 
     def emulate(self, test_data, test_target):
         z = [(x, int(y1)) for x, y1 in zip(np.argmax(self.predict(x=test_data), axis=1), test_target)]
         zs = np.sum(int(a == b) for a, b in z)
-        print zs
+        print(zs)
 
 
